d2a4e5a7b197c48987ce8f5a46fea0e483a7c4af
a.klopp <a.klopp@criteo.com>
2023-02-23 18:08:34 +0100

    Support numbers in cy.wait

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,9 @@
 import { AddAwaitedRequest } from './request-manager';
 
 Cypress.Commands.overwrite<'wait', 'optional'>('wait', (originalFn, alias, options) => {
-  const optionList = typeof options === 'string' ? [options] : options;
-  AddAwaitedRequest(optionList);
+  if (typeof options !== 'number') {
+    const optionList = typeof options === 'string' ? [options] : options;
+    AddAwaitedRequest(optionList);
+  }
   return originalFn(alias, options);
 });
